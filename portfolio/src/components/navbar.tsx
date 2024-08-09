d1d5e5d48efe05e65f18f249f9e48f2c44143501
WillianCosta <willian95@outlook.com>
2024-08-09 17:03:27 -0300

    feat: adição de navbar

---
+++ portfolio/src/components/navbar.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-custom-dark-blue">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-custom-dark-blue rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a href="/">Sobre</a>
+            </li>
+            <li>
+              <a href="/">Projetos</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl text-custom-green" href="/">
+          Willian Costa
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="/">Sobre</a>
+          </li>
+          <li>
+            <a href="/">Projetos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
