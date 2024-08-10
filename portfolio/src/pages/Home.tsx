1dae3094d379086931bc36a3330cb2f469aa33cc
WillianCosta <willian95@outlook.com>
2024-08-10 16:32:02 -0300

    fix: correção de tamanho da tela home

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -4,10 +4,11 @@ import Sobre from "../components/Sobre";
 import Social from "../components/Social";
 import Skills from "../components/Skills";
 import Projetos from "../components/Projetos";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="hero min-h-screen relative h-screen">
+    <div className="hero min-h-screen relative">
       <div className="hero-content">
         <div className="max-w-screen">
           <Banner />
