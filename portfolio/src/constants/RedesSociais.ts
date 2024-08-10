2c6024a6d52b7d3da4c1cf66712882a32606ef8f
WillianCosta <willian95@outlook.com>
2024-08-10 12:24:06 -0300

    feat: adição de constant para redes sociais

---
+++ portfolio/src/constants/RedesSociais.ts
@@ -0,0 +1,28 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faSquareBehance
+} from "@fortawesome/free-brands-svg-icons";
+
+export const SOCIAL_MEDIA = [
+  {
+    id: 1,
+    name: "Linkdin",
+    url: "https://www.linkedin.com/in/willianocosta/",
+    icon: faLinkedin
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    url: "https://github.com/costaowillian",
+    icon: faGithub
+  },
+  {
+    id: 3,
+    name: "Behance",
+    url: "https://www.behance.net/costawillian",
+    icon: faSquareBehance
+  }
+];
