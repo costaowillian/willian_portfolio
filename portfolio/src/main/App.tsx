120d1e966b99877ea93fac39c3547713073b021b
WillianCosta <willian95@outlook.com>
2024-08-10 16:31:43 -0300

    feat: adiçãod e componenete footer no app

--- portfolio/src/main/App.tsx
+++ portfolio/src/main/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Home from "../pages/Home";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
