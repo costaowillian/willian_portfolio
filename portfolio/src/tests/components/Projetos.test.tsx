11e761d2b826777318474228b02a85ccdedc133c
WillianCosta <willian95@outlook.com>
2024-08-11 10:35:48 -0300

    test: teste do componennte Projetos

---
+++ portfolio/src/tests/components/Projetos.test.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+import { render, screen } from "@testing-library/react";
+import { motion, useInView } from "framer-motion";
+import Projetos from "../../components/Projetos";
+import { PROJETOS } from "../../constants/ProjetosConstant";
+
+jest.mock("framer-motion", () => ({
+  ...jest.requireActual("framer-motion"),
+  motion: {
+    div: ({ children }: { children: ReactNode }) => <div>{children}</div>
+  }
+}));
+
+jest.mock("react-intersection-observer", () => ({
+  useInView: () => ({
+    ref: jest.fn(),
+    inView: true
+  })
+}));
+
+describe("Projetos Componet", () => {
+  test("Deve renderizar o título e todos os projetos com suas respectivas tecnologias", () => {
+    render(<Projetos />);
+
+    const title = screen.getByText("Últimos projetos desenvolvidos");
+    expect(title).toBeInTheDocument();
+
+    PROJETOS.forEach((projeto) => {
+      const projectTitle = screen.getByText(projeto.title);
+      expect(projectTitle).toBeInTheDocument();
+
+      const projectDecription = screen.getByText(projeto.descricao);
+      expect(projectDecription).toBeInTheDocument();
+
+      projeto.tecnologias.forEach((tech) => {
+        const techBadges = screen.getAllByText(tech.name);
+        expect(techBadges.length).toBeGreaterThan(0);
+      });
+    });
+  });
+});
