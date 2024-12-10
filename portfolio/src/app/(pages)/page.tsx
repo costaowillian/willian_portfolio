8322bd8bbe09fb3763966e54e91ad51e36652577
WillianCosta <willian95@outlook.com>
2024-12-10 17:16:16 -0300

    feat: add HomeBanner component

--- portfolio/src/app/(pages)/page.tsx
+++ portfolio/src/app/(pages)/page.tsx
@@ -1,11 +1,12 @@
+import HomeBanner from "@/components/HomeBanner/HomeBanner";
+
 export default function Home() {
   return (
-    <div className="hero min-h-screen relative">
-      <div className="hero-content">
-        <div className="max-w-screen">
-          <p className="text-primary">Home Works</p>
-        </div>
-      </div>
+    <div className="container">
+      <section className="mx-4 sm:mx-0">
+        <HomeBanner size="lg" />
+        <p className="text-primary">Home Works</p>
+      </section>
     </div>
   );
 }
--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -21,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${ubuntu.className} antialiased`}>{children}</body>
+      <body
+        className={`${ubuntu.className} antialiased flex flex-col pb-[90px] sm:pb-0`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
