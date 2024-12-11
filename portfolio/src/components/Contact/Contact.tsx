c8a0a6688e96ca8ba2bf0f463e7269514112382e
WillianCosta <willian95@outlook.com>
2024-12-11 17:56:13 -0300

    feat: add ProfileContent component

--- portfolio/src/components/Contact/Contact.tsx
+++ portfolio/src/components/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import CustomIconLink from "../CustomIconLink/CustomIconLink";
 
 export default function Contact() {
   return (
-    <div className="flex flex-col sm:flex-row justify-between">
+    <div className="flex flex-col sm:flex-row justify-between gap-4">
       <h2 className="text-4xl sm:text-5xl w-full font-bold">Contato</h2>
 
       <div className="flex items-center justify-center">
